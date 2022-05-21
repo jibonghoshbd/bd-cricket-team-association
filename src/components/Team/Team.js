b49d85e7b9ab8b04f11c6b6c4daf4f4db92ebd2c
jibonghoshbd <jibonghosh1510@gmail.comm>
2022-05-21 15:11:43 +0600

    favion added

---
+++ src/components/Team/Team.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Team.css'
+
+const Team = () => {
+    return (
+        <div>
+            <h1>This is team</h1>
+        </div>
+    );
+};
+
+export default Team;
